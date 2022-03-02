9ba0964426a67bfbefa17a65b0bdc44b0a437cda
Stan <stantonmoore42@gmail.com>
2022-03-02 12:19:29 -0500

    Tip Calculator

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/greeting/:name', (req, res) => {
     res.send('hello ' + req.params.name);
 });
 
+app.get('/tip/:total/:tip', (req, res) => {
+    res.send('' + .01 * req.params.total * req.params.tip);
+});
+
 app.listen(3000, () => {
     console.log('listening');
 });
