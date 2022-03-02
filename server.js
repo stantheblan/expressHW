c7e1e93230fb7c277c76f855f2bffd17fbb1a0eb
Stan <stantonmoore42@gmail.com>
2022-03-02 13:04:25 -0500

    8ball

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+let responses = ["It is certain", "It is decidedly so", "Without a doubt", 
+"Yes definitely","You may rely on it", "As I see it yes", "Most likely", 
+"Outlook good","Yes", "Signs point to yes", "Reply hazy try again", 
+"Ask again later","Better not tell you now", "Cannot predict now", 
+"Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+
 app.get('/', (req, res) => {
     res.send('Home Page');
 });
@@ -11,6 +18,11 @@ app.get('/greeting/:name', (req, res) => {
 
 app.get('/tip/:total/:tip', (req, res) => {
     res.send('' + .01 * req.params.total * req.params.tip);
+});
+
+app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+    let r = responses[Math.floor(Math.random()*responses.length)];
+    res.send('' + r);
 });
 
 app.listen(3000, () => {
